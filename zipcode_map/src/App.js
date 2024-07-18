f4d7f5331a327e1c7de3a1d91370ae3899494b83
tanaeta <takuma.tanae@gmail.com>
2024-07-19 07:52:47 +0900

    change list view

--- zipcode_map/src/App.js
+++ zipcode_map/src/App.js
@@ -2,7 +2,10 @@ import React, { useState } from 'react';
 import MapComponent from './components/MapComponent.js';
 
 const App = () => {
-  const [geoJson, setGeoJson] = useState(null);
+  const [geoJson, setGeoJson] = useState({ features: [] });
+  const [postalCodes, setPostalCodes] = useState([]);
+  // 選択された郵便番号の詳細情報を保持するための状態
+  const [selectedPostalCode, setSelectedPostalCode] = useState(null);
 
   const handleShapeDrawn = async (geoJsonData) => {
     setGeoJson(geoJsonData);
@@ -11,7 +14,6 @@ const App = () => {
 
   const fetchPostalCodes = async (geoJson) => {
     try {
-      //const response = await fetch('/api/postalcodes', {
       const response = await fetch('http://localhost:3001/api/postalcodes', {
         method: 'POST',
         headers: {
@@ -23,16 +25,36 @@ const App = () => {
         throw new Error(`Error: ${response.status}`);
       }
       const data = await response.json();
-      console.log(data);
+      setPostalCodes(data);
     } catch (error) {
       console.error('Failed to fetch postal codes:', error);
     }
   };
 
+  // 郵便番号がクリックされたときの処理
+  const handlePostalCodeClick = (postalCode) => {
+    setSelectedPostalCode(postalCode);
+  };
+
   return (
     <div>
       <MapComponent onShapeDrawn={handleShapeDrawn} />
-      {geoJson && <pre>{JSON.stringify(geoJson, null, 2)}</pre>}
+      {/* 郵便番号をリストで表示 */}
+      <ul>
+        {postalCodes.map((postalCode, index) => (
+          <li key={index} onClick={() => handlePostalCodeClick(postalCode)}>
+            {postalCode.id}
+          </li>
+        ))}
+      </ul>
+      {/* 選択された郵便番号の詳細情報を表示 */}
+      {selectedPostalCode && (
+        <div>
+          <h2>郵便番号詳細</h2>
+          <p>ID: {selectedPostalCode.id}</p>
+          {/* 他の詳細情報もここに表示 */}
+        </div>
+      )}
     </div>
   );
 };
